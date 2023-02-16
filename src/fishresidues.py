9a5e6c0e6aeb00f4906a0b25d2d1ce4301eba6f9
Carlos Vigil Vásquez <carlos.vigil.v@gmail.com>
2023-02-16 14:39:58 -0300

    feat: added scene creation
    Co-authored-by: Joaquín Gutierrez <jmgutierrez4@uc.cl>
    Co-authored-by: Carlos Vigil-Vásquez <carlos.vigil.v@gmail.com>

--- src/fishresidues.py
+++ src/fishresidues.py
@@ -33,30 +33,51 @@ def main():
     cmd.load(query)
     cmd.delete("conservation")
     query_name = cmd.get_names("objects")[0]
-    for target in targets:
-        cmd.load(target)
-
-    # Generate selections and color
-    for group, resn in AA_GROUPS.items():
-        cmd.select("resn " + resn)
-        cmd.set_name("sele", group)
-        cmd.color(AA_COLORS[group], group)
-
-    # "Fish" residues
-    for group in AA_GROUPS.keys():
-        query_residues= f"({query_name} and conservation_gt75_le100 and {group} and name CA)"
-        target_residues = f"(*_t and {group} and name CA)"
-        cmd.select(f"byres {query_residues} within {str(TOLERANCE)} of {target_residues}")
-        cmd.set_name("sele", f"fished_{group}")
-
-    cmd.select(" + ".join([f"fished_{group}" for group in AA_GROUPS.keys()]))
-    cmd.set_name("sele", "fished_aa")
-
-    # Set style
-    cmd.show("lines", "conservation_gt75_le100")
-    cmd.show("sticks", "fished_aa")
+    cmd.show_as("cartoon")
+    cmd.cartoon("automatic")
     cmd.set("cartoon_transparency", 0.5, "all")
-    cmd.bg_color("white")
+    for target in targets:
+        query_file = sys.argv[2] + "/" + query_name.replace("_","") + "_" + "_".join(target.split("/")[-1].split("_")[1:-1]) + "_q.pdb"
+        query_frag = query_name.replace("_","") + "_" + "_".join(target.split("/")[-1].split("_")[1:-1]) + "_q"
+        target_frag = target.strip('.pdb').split('/')[-1]
+        fragment_name = "_".join(target.split("/")[-1].split("_")[1:-1])
+        cmd.load(query_file)
+        cmd.load(target)
+        query_residues1= f"({query_name} and conservation_gt50_le75 and name CA)"
+        cmd.select(f"byres {query_frag} within 0.1 of {query_residues1}")
+        cmd.set_name("sele", "conservation_gt50_le75_" + query_frag)
+        query_residues2= f"({query_name} and conservation_gt75_le100 and name CA)"
+        cmd.select(f"byres {query_frag} within 0.1 of {query_residues2}")
+        cmd.set_name("sele", "conservation_gt75_le100_" + query_frag)
+        # Colors and physico-chemical groups
+        for group, resn in AA_GROUPS.items():
+            cmd.select(f"resn {resn} and {query_frag}")
+            cmd.set_name("sele", f"{group}_{query_frag}")
+            cmd.color(AA_COLORS[group], f"{group}_{query_frag}")
+            cmd.select(f"resn {resn} and {target_frag}")
+            cmd.set_name("sele", f"{group}_{target_frag}")
+            cmd.color(AA_COLORS[group], f"{group}_{target_frag}")
+        # "Fish" residues
+        for group in AA_GROUPS.keys():
+            query_residues= f"({query_frag} and conservation_gt75_le100_{query_frag} and {group}_{query_frag} and name CA)"
+            target_residues = f"({target_frag} and {group}_{target_frag} and name CA)"
+            cmd.select(f"byres {target_residues} within {str(TOLERANCE)} of {query_residues}")
+            cmd.set_name("sele", f"fished_{group}_{fragment_name}")
+            cmd.show("lines", f"byres {query_residues} within {str(TOLERANCE)} of fished_{group}_{fragment_name}")
+            cmd.show("sticks", f"fished_{group}_{fragment_name}")
+            cmd.set("stick_transparency", 0.5, "all")
+        cmd.delete(f"*_aa_{target_frag}")
+        cmd.delete(f"*_aa_{query_frag}")
+        cmd.scene(f"Fragments_{fragment_name}", "store", f"Residues that show more than 75% of conservation while maintaining physicochemical properties\nare shown in lines (query) and sticks (target).")
+        cmd.disable("all")
+
+    cmd.scene(f"Fragments_{fragment_name}", "recall")
+    cmd.delete(query_name)
+    cmd.delete("conservation_gt75_le100")
+    cmd.delete("conservation_gt50_le75")
+    cmd.delete("conservation_gt25_le50")
+    cmd.delete("conservation_gt0_le25")
+    cmd.delete("conserved_gap")
 
     # Save session
     cmd.deselect()
